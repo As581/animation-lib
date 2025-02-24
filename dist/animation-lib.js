70a0f675e09caeae1bf505680bc1b622036f8568
SingleThreaded400 <av0232016@gmail.com>
2025-02-24 11:29:46 +0530

    Add files via upload

--- dist/animation-lib.js
+++ dist/animation-lib.js
@@ -1,1 +1,14 @@
-
+function a(t, s = 1e3) {
+  t.style.opacity = 0, t.style.transition = `opacity ${s}ms ease-in-out`, setTimeout(() => {
+    t.style.opacity = 1;
+  }, 10);
+}
+function o(t, s = 1e3) {
+  t.style.transform = "translateX(-100%)", t.style.transition = `transform ${s}ms ease-in-out`, setTimeout(() => {
+    t.style.transform = "translateX(0)";
+  }, 10);
+}
+export {
+  a as fadeIn,
+  o as slideIn
+};

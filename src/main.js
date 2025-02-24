85d6da8960e1764079548dd3c5f43ab90130dc02
SingleThreaded400 <av0232016@gmail.com>
2025-02-24 11:28:28 +0530

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
+export function slideFromLeft(element, duration = 1000) {
+    element.style.transform = "translateX(-100%)";
+    element.style.transition = `transform ${duration}ms ease-in-out`;
+    setTimeout(() => {
+        element.style.transform = "translateX(0)";
+    }, 10);
+}
 
+export function slideFromTop(element, duration = 1000) {
+    element.style.transform = "translateY(-100%)";
+    element.style.transition = `transform ${duration}ms ease-in-out`;
+    setTimeout(() => {
+        element.style.transform = "translateY(0)";
+    }, 10);
+}
